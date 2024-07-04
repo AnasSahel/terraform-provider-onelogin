cf6389855d9691d0cb3e61e264a375f7385a4f30
Anas SAHEL <anas.sahel@gmail.com>
2024-07-04 15:20:23 +0000

    feat: setup the devcontainer

---
+++ .devcontainer/scripts/post-create.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+
+echo "export GOBIN=$HOME/go/bin" >> ~/.bashrc
+source ~/.bashrc
+
+# Create the .terraformrc file with the specified content
+cat <<EOL > ~/.terraformrc
+provider_installation {
+  dev_overrides {
+      "hashicorp.com/edu/hashicups" = "$GOBIN"
+  }
+
+  direct {}
+}
+EOL
+
+echo "GOBIN is set to $GOBIN"
+echo ".terraformrc file created in the home directory with the specified content"
